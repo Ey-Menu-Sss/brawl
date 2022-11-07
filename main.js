ddf58da3bb2d20e426993d67574f3271c0fd7a57
muhammadqodir <dilnavo3@gmail.com>
2022-11-07 23:22:39 +0500

    brawl

---
+++ main.js
@@ -0,0 +1,12 @@
+// let answer = +prompt("1-10 gacha bo'lgan random son yzing");
+// let random = Math.floor(Math.random() * 10);
+
+// //
+
+// if (answer == random) {
+//   console.log("To'gri");
+// }
+//  else {
+//   console.log("No'to'gri random son " + random + " edi");
+// }
+// console.log(random);
